d712bbe51b6cf4b7006a921158812ad00137ea5e
jarek3660-cmd <jarek3660@gmail.com>
2026-03-10 13:59:00 -0600

    Create D_latch.vhdl

---
+++ D_latch.vhdl
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/10/2026 01:43:25 PM
+-- Design Name: 
+-- Module Name: D-Latch - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity D_Latch is
+    Port ( D, CLK : in STD_LOGIC;
+           Q, Dummy : out STD_LOGIC);
+end D_Latch;
+
+architecture Behavioral of D_Latch is
+
+begin
+state: process(D,CLK)
+begin
+    if CLK = '1' then 
+        Q <= D;
+    else
+        Dummy <= D;
+    end if;
+end process;
+end Behavioral;
